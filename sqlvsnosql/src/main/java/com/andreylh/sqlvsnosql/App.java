9b988226bd33407e0e26287033edd92a6edae5fb
Andrey <andreylh@gmail.com>
2015-09-15 22:45:22 -0300

    read files from disk

--- sqlvsnosql/src/main/java/com/andreylh/sqlvsnosql/App.java
+++ sqlvsnosql/src/main/java/com/andreylh/sqlvsnosql/App.java
@@ -1,14 +1,18 @@
 package com.andreylh.sqlvsnosql;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import com.andreylh.sqlvsnosql.trajectory.Trajectory;
+
 /**
  * Hello world!
  *
  */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
+	public static void main(String[] args) throws IOException {
 		
-		
+		Trajectory.createTrajectoriesFromFiles(Paths.get("D:/Andrey/Research/Database"));	
 	}
 }
 
